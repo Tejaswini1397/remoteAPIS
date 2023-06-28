c12511982dbdfb129d19373f4846f3a21619ba7c
Tejaswini Khairnar <tejaswinikhairnar97@gmail.com>
2023-06-28 12:59:21 +0530

    new

---
+++ src/main/java/com/example/remoteAPI/RemoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.remoteAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/remoteAPI/RemoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.remoteAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
